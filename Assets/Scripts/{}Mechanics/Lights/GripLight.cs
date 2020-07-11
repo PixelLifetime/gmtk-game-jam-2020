284f224971f7f3ca6361ee7abc281fc9d2c7f361
Sergio Oporto <ser.op.te@gmail.com>
2020-07-10 22:08:26 -0400

    Add scripts to manage player's grip

---
+++ Assets/Scripts/{}Mechanics/Lights/GripLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripLight : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _maxGripIncrement = 10f;
+    [SerializeField]
+    private float _minimumReachRadius = 1f;
+    [SerializeField]
+    private float _maximumReachRadius = 2f;
+    [SerializeField]
+    private AnimationCurve _incrementMultiplierCurve = AnimationCurve.Linear(0,0,1,1);
+
+    /// <summary>
+    /// Gets how much the grip will increment, depending of the distance between the player and the light
+    /// </summary>
+    /// <param name="target">The player's position</param>
+    /// <returns></returns>
+    public float GetGripIncrement(Transform target)
+    {
+        float distanceToTarget = Vector2.Distance(target.position, transform.position);
+        if (distanceToTarget <= _minimumReachRadius)
+        {
+            return _maxGripIncrement;
+        }
+        if (distanceToTarget >+ _maximumReachRadius)
+        {
+            return 0f;
+        }
+        return _incrementMultiplierCurve.Evaluate(
+            (distanceToTarget - _maximumReachRadius) / (_minimumReachRadius - _maximumReachRadius)) 
+            * _maxGripIncrement;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _minimumReachRadius);
+        Gizmos.DrawWireSphere(transform.position, _maximumReachRadius);
+    }
+}
---
+++ Assets/Scripts/{}Mechanics/Player/PlayerGrip.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrip : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxGrip;
+    [SerializeField]
+    private float _gripDecrement;
+    private List<GripLight> _currentLights;
+    private float _grip;
+    void Start()
+    {
+        _grip = _maxGrip;
+        _currentLights = new List<GripLight>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentLights.Count == 0)
+        {
+            _grip = Mathf.Max(_grip - _gripDecrement, 0f);
+            Debug.Log("My grip is going down");
+        }
+        else
+        {
+            var increment = 0f;
+            foreach(var light in _currentLights)
+            {
+                increment = Mathf.Max(increment, light.GetGripIncrement(transform));
+            }
+            Debug.Log("My grip is going up by " + increment);
+            _grip = Mathf.Min(_grip + increment, _maxGrip);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var light = other.gameObject.GetComponent<GripLight>();
+        if (light != null)
+        {
+            _currentLights.Add(light);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var light = other.gameObject.GetComponent<GripLight>();
+        if (light != null)
+        {
+            _currentLights.Remove(light);
+        }
+    }
+}
