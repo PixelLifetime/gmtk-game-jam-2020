9b993acaf8a659dc821e391f398fc1f7a304039e
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-11 16:40:45 +0300

    Add base for audio control. Add ui package explicitly.

---
+++ Assets/Scripts/{}Audio/AudioSourceControlGroup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "[Audio Source Control Group]", menuName = "[Audio]/[Audio Source Control Group]")]
+public class AudioSourceControlGroup : ScriptableObject
+{
+}
---
+++ Assets/Scripts/{}Audio/AudioSourceController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour, IPoolable
+{
+	public GameObject GameObject => throw new System.NotImplementedException();
+
+	public ObjectPool.Pool Pool { get; set; }
+
+	public void OnRelease()
+	{
+	}
+
+	public void OnAquire()
+	{
+	}
+}
