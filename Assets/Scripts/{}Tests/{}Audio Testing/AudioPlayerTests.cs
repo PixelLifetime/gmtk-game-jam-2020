ed9b50aadac547d22b57c28bc5d4eb545420e374
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-11 21:24:16 +0300

    Add singleton AudioPlayer. Add tests for AudioPlayer. Set up scene.

---
+++ Assets/Scripts/{}Tests/{}Audio Testing/AudioPlayerTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayerTests : MonoBehaviour
+{
+	[SerializeField] private AudioClipArchive _audioClipArchiveAmbience;
+	public AudioClipArchive _AudioClipArchiveAmbience => this._audioClipArchiveAmbience;
+
+	[SerializeField] private AudioClipArchive _audioClipArchiveMusic;
+	public AudioClipArchive _AudioClipArchiveMusic => this._audioClipArchiveMusic;
+
+	[SerializeField] private AudioClipArchive _audioClipArchiveSoundEffect;
+	public AudioClipArchive _AudioClipArchiveSoundEffect => this._audioClipArchiveSoundEffect;
+
+	private IEnumerator Start()
+	{
+		yield return new WaitForSecondsRealtime(1.0f);
+
+		while (true)
+		{
+			AudioClip audioClip;
+			AudioSourceController audioSourceController;
+
+			// Ambience
+			audioClip = this._audioClipArchiveAmbience.Random();
+			audioSourceController = AudioPlayer.Instance.Play(audioClip: audioClip, type: AudioPlayer.Type.Ambience);
+
+			yield return new WaitForSecondsRealtime(audioClip.length + 0.5f);
+
+			ObjectPool.Instance.Release(audioSourceController);
+
+			// Music
+			audioClip = this._audioClipArchiveMusic.Random();
+			audioSourceController = AudioPlayer.Instance.Play(audioClip: audioClip, type: AudioPlayer.Type.Music);
+
+			yield return new WaitForSecondsRealtime(audioClip.length + 0.5f);
+
+			ObjectPool.Instance.Release(audioSourceController);
+
+			// Sound Effect
+			audioClip = this._audioClipArchiveSoundEffect.Random();
+			audioSourceController = AudioPlayer.Instance.Play(audioClip: audioClip, type: AudioPlayer.Type.SoundEffect);
+
+			yield return new WaitForSecondsRealtime(audioClip.length + 0.5f);
+
+			ObjectPool.Instance.Release(audioSourceController);
+		}
+	}
+}
