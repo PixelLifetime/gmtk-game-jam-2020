f365d3e10f7d262e1a514f9623ebe3e6e170c2da
Sergio Oporto <ser.op.te@gmail.com>
2020-07-11 00:26:13 -0400

    Add scripts for controlling platforms that trigger depending on grip value

---
+++ Assets/Scripts/{}Mechanics/Platforms/GripPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GripPlatform : GripEffect
+{
+    public new Collider2D collider2D;
+    public Tilemap tilemap;
+    
+    protected override void DoEffect()
+    {
+        collider2D.enabled = false;
+        tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 0f);
+    }
+
+    protected override void UndoEffect()
+    {
+        collider2D.enabled = true;
+        tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 1f);
+    }
+}
