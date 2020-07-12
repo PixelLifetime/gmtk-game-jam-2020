8be3ad2a19808ca52f9baeaeb94e49a8d4f62ee5
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-12 17:14:20 +0300

    Add tooltip.

---
+++ Assets/Scripts/Dump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dump : MonoBehaviour
+{
+	private void Start()
+	{
+		TooltipView.Instance.Display(position: this.transform.position, text: "Press\nE");
+	}
+}
