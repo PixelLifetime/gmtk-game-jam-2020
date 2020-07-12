59d2a7ba579074aaa02265cee2165588cace17f2
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-12 18:35:29 +0300

    Fix AudioPlayer problem with object pooling.

--- Assets/Scripts/{}Audio/AudioClipPlaybackEngine.cs
+++ Assets/Scripts/{}Audio/AudioClipPlaybackEngine.cs
@@ -10,37 +10,56 @@ public class AudioClipPlaybackEngine : MonoBehaviour
 	[SerializeField] private AudioSourcePlayer _audioSourcePlayer;
 	public AudioSourcePlayer _AudioSourcePlayer => this._audioSourcePlayer;
 
+	private IEnumerator PlayProcess()
+	{
+		AudioSourceController audioSourceController = this._audioSourcePlayer.Play(
+			audioClip: this._audioClip
+		);
+
+		yield return new WaitForSecondsRealtime(this._audioClip.length);
+
+		ObjectPool.Instance.Release(audioSourceController);
+	}
+
+	public void Play() => this.StartCoroutine(this.PlayProcess());
+
+	[SerializeField] private bool _playOnStart;
+	public bool _PlayOnStart => this._playOnStart;
+
 	[SerializeField] private bool _loop;
 	public bool _Loop => this._loop;
 
 	private IEnumerator Start()
 	{
-		AudioSourceController audioSourceController = this._audioSourcePlayer.AquireAudioSourceController();
-
-		//! Calling `Play` each iteration.
-		do
+		if (this._playOnStart)
 		{
-			audioSourceController.Play(
-				audioClip: this._audioClip
-			);
+			AudioSourceController audioSourceController = this._audioSourcePlayer.AquireAudioSourceController();
 
-			yield return new WaitForSecondsRealtime(this._audioClip.length);
+			//! Calling `Play` each iteration.
+			do
+			{
+				audioSourceController.Play(
+					audioClip: this._audioClip
+				);
+
+				yield return new WaitForSecondsRealtime(this._audioClip.length);
+			}
+			while (this._loop);
+
+			ObjectPool.Instance.Release(audioSourceController);
+
+			//? How to do it without calling `Play` each iteration?
+			//? Why is it not recommended in this cae?
+			//!? Because you are modifything a state of an entity that has been pooled.
+			//!? It sure seems an easier and a better choice, but it may lead to unexpected bugs like something playing again when it shouldn't have.
+
+			//audioSourceController.AudioSource.loop = this._loop;
+
+			//audioSourceController.Play(
+			//	audioClip: this._audioClip
+			//);
+
+			//yield return new WaitForSecondsRealtime(this._audioClip.length);
 		}
-		while (this._loop);
-
-		ObjectPool.Instance.Release(audioSourceController);
-
-		//? How to do it without calling `Play` each iteration?
-		//? Why is it not recommended in this cae?
-		//!? Because you are modifything a state of an entity that has been pooled.
-		//!? It sure seems an easier and a better choice, but it may lead to unexpected bugs like something playing again when it shouldn't have.
-
-		//audioSourceController.AudioSource.loop = this._loop;
-
-		//audioSourceController.Play(
-		//	audioClip: this._audioClip
-		//);
-
-		//yield return new WaitForSecondsRealtime(this._audioClip.length);
 	}
 }
--- Assets/Scripts/{}Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/{}Audio/AudioSourcePlayer.cs
@@ -7,14 +7,14 @@ public class AudioSourcePlayer : MonoBehaviour
 	[SerializeField] private AudioSourceController _audioSourceControllerPrefab;
 	public AudioSourceController _AudioSourceControllerPrefab => this._audioSourceControllerPrefab;
 
-	public void PlayUI(AudioClip audioClip)
-	{
-		AudioSourceController audioSourceController = ObjectPool.Instance.Aquire<AudioSourceController>(
-			poolable: this._audioSourceControllerPrefab
-		);
+	//public void PlayUI(AudioClip audioClip)
+	//{
+	//	AudioSourceController audioSourceController = ObjectPool.Instance.Aquire<AudioSourceController>(
+	//		poolable: this._audioSourceControllerPrefab
+	//	);
 
-		audioSourceController.Play(audioClip: audioClip);
-	}
+	//	audioSourceController.Play(audioClip: audioClip);
+	//}
 
 	public AudioSourceController Play(AudioClip audioClip)
 	{
