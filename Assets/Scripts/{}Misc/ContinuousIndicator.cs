46267695e86037043cd439fb5c4a95613c01f343
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-12 15:19:27 +0300

    Set up skeleton of HUD. Add Indicators for controlling HUD elements.

---
+++ Assets/Scripts/{}Misc/ContinuousIndicator.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousIndicator : Indicator<float, UnityEventFloat>
+{
+	protected override float Clamp(float value, float min, float max) => Mathf.Clamp(value: value, min: min, max: max);
+}
