b9f92c44c3db984229bc93c76d0bd729361f3095
Pixel Lifetime <maxymkarpinsky@gmail.com>
2020-07-12 15:48:28 +0300

    Add default events. Add Notes.

---
+++ Assets/_Specific/{}Views/NoteView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NoteView : View
+{
+	[SerializeField] private TextMeshProUGUI _textField;
+	public TextMeshProUGUI _TextField => this._textField;
+
+	public void Display(Note note)
+	{
+		this._textField.text = note._Text;
+
+		//TODO: pause the game.
+
+		this.Open();
+		this.Show();
+	}
+
+	public override void Hide()
+	{
+		base.Hide();
+
+		//TODO: unpause the game.
+	}
+}
